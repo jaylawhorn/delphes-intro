1556fdc26056bb7baf3b762a72ca89b93a060b29
Jay <jlawhorn@mit.edu>
2018-07-16 18:20:24 +0200

    analysis macro doesn't quite work yet

--- SignalExtraction/analysis.C
+++ SignalExtraction/analysis.C
@@ -2,54 +2,108 @@
 #include <TSystem.h>
 #include <TFile.h>
 #include <TTree.h>
+#include <TObject.h>
 #include <TClonesArray.h>
 #include <TVector2.h>
 #include <TMath.h>
 #include <TChain.h>
 #include <TH1.h>
+#include <TH2.h>
 #include <TCanvas.h>
 #include <vector>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include "TLorentzVector.h"
-
 using namespace std;
 
 Float_t deltaR( const Float_t eta1, const Float_t eta2, const Float_t phi1, const Float_t phi2 );
-
 Float_t deltaPhi( const Float_t phi1, const Float_t phi2 );
 
-void analysis(const TString inputfile="/afs/cern.ch/work/j/jlawhorn/public/delphes-sel-test/ZZZ/ZZZ_TuneCUETP8M1_14TeV-amcatnlo-pythia8_10_0.root") {
+class TLepton : public TObject {
+public:
+  TLepton(float pt, float eta, float phi, int charge, int pid, float iso):
+    PT(pt), Eta(eta), Phi(phi), Charge(charge), PID(pid), IsolationVar(iso)
+  {}
+
+  ~TLepton(){}
+
+  float PT; float Eta; float Phi; int Charge; int PID; float IsolationVar;
+
+  ClassDef(TLepton,1);
+};
+
+class THadJet : public TObject {
+public:
+  THadJet(float pt, float eta, float phi, float mass, int btag):
+    PT(pt), Eta(eta), Phi(phi), Mass(mass), BTag(btag)
+  {}
+
+  ~THadJet(){}
+
+  float PT; float Eta; float Phi; float Mass; int BTag;
+
+  ClassDef(THadJet,1);
+};
+
+void analysis(const TString inputfile="test.root") {
 
   Float_t eventWeight;
+  uint nEl, nMu, nJet, nBJet;
+  Float_t metPt;
+  Float_t metPhi;
 
-  Float_t ptMu1, etaMu1, phiMu1, mMu1;
-
-  Float_t ptMu2, etaMu2, phiMu2, mMu2;
-
-  Float_t mZ;
-
+  TClonesArray *vEl  = new TClonesArray("TLepton");
+  TClonesArray *vMu  = new TClonesArray("TLepton");
+  TClonesArray *vJet = new TClonesArray("THadJet");
+  
   TFile *inFile = new TFile(inputfile, "READ");
   TTree *inTree = (TTree*) inFile->Get("Events");
   inTree->SetBranchAddress("eventWeight", &eventWeight);
-  inTree->SetBranchAddress("ptMu1",  &ptMu1);
-  inTree->SetBranchAddress("etaMu1", &etaMu1);
-  inTree->SetBranchAddress("phiMu1", &phiMu1);
-  inTree->SetBranchAddress("mMu1",   &mMu1);
-  inTree->SetBranchAddress("ptMu2",  &ptMu2);
-  inTree->SetBranchAddress("etaMu2", &etaMu2);
-  inTree->SetBranchAddress("phiMu2", &phiMu2);
-  inTree->SetBranchAddress("mMu2",   &mMu2);
-  inTree->SetBranchAddress("mZ",     &mZ);
+  inTree->SetBranchAddress("nEl", &nEl);
+  inTree->SetBranchAddress("nMu", &nMu);
+  inTree->SetBranchAddress("nJet", &nJet);
+  inTree->SetBranchAddress("nBJet", &nBJet);
+  inTree->SetBranchAddress("metPt", &metPt);
+  inTree->SetBranchAddress("metPhi", &metPhi);
 
+  inTree->SetBranchAddress("vEl", &vEl);
+  inTree->SetBranchAddress("vMu", &vMu);
+  inTree->SetBranchAddress("vJet", &vJet);
+
+  //      no spaces  here V or here V
+  TH1D *hNLep = new TH1D("hNLep", "hNLep", 15, 0, 15);
+  TH1D *hNJet = new TH1D("hNJet", "hNJet", 15, 0, 15);
+  TH2D *hNvN  = new TH2D("hNvN",  "hNvN", 15, 0, 15, 15, 0, 15);
+
+  TH1D *hPtEle1 = new TH1D("hPtEle1", "hPtEle1", 25, 0, 250);
 
   for (int i=0; i<inTree->GetEntries(); i++) {
     inTree->GetEntry(i);
 
-    // continue as normal
-    
+    hNLep->Fill(vEl->GetEntries()+vMu->GetEntries());
+    hNJet->Fill(vJet->GetEntries());
+    hNvN->Fill(vEl->GetEntries()+vMu->GetEntries(), vJet->GetEntries());
+
+    //TLepton *ele1 = (TLepton*)vEl->At(0);
+    cout << ((TLepton*)vEl->At(0))->PT << endl;
+    hPtEle1->Fill(ele1->PT);
+
   }  
+
+  TCanvas *c = new TCanvas("c","c",800, 600);
+
+  hNLep->Draw("");
+  c->SaveAs("nLep.png");
+
+  hNJet->Draw("");
+  c->SaveAs("nJet.png");
+
+  hNvN->Draw("colz");
+  c->SaveAs("nvn.png");
+
+  hPtEle1->Draw("");
+  c->SaveAs("ptEle1.png");
 
   
 }
